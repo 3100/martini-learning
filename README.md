7596904d64c4509c33876982eae5b388fb20153a
3100 <sugar16g@gmail.com>
2014-06-04 02:05:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+my learnings in `martini`
+---
+
+~~~
+$ go get "github.com/go-martini/martini"
+$ go get "github.com/yossi/rendergold"
+$ go run server.go
+~~~
+
+* `rendergold` needs `hg`
---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "github.com/go-martini/martini"
+  "github.com/yosssi/rendergold"
+)
+
+func main() {
+  m := martini.Classic()
+  // reads "templates" directory by default
+  m.Use(rendergold.Renderer())
+
+  m.Get("/", func(r rendergold.Render) {
+    // parses "templates/top.gold"
+    r.HTML(200, "top", nil)
+  })
+
+  m.Run()
+}
